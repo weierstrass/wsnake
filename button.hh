7a418cf2121fba7e730c70aad237f4c851ea2ffc
Weierstrass <andreas@bulling.se>
2012-03-10 16:32:09 +0100

    add a button class

---
+++ button.hh
@@ -0,0 +1,31 @@
+
+#ifndef _inc_button_
+#define _inc_button_
+
+using namespace std;
+
+#include <string>
+#include <iostream>
+#include "SDL/SDL.h"
+#include "SDL/SDL_ttf.h"
+#include "const.hh"
+#include "aux.hh"
+
+class Button{
+private:
+  string text;
+  SDL_Surface *surf;
+  TTF_Font *font;
+  int id;
+  bool highlighted;
+public:
+  SDL_Rect bounds;
+  Button(int x, int y, int w, int h, string text, 
+	 TTF_Font *f, int id);
+  SDL_Surface* getSurface();
+  string getText();
+  int getID();
+  void setHighlighted(bool b);
+};
+
+#endif
