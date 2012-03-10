325c00578b352081699204f97b91b8453d21f032
Weierstrass <andreas@bulling.se>
2012-03-10 11:54:45 +0100

    added file for auxiliary

---
+++ aux.cc
@@ -0,0 +1,27 @@
+/*Auxilary routines*/
+
+#include "aux.hh"
+
+/*Loads image from file and convert it to proper format*/
+SDL_Surface *loadImage(string filename){
+  SDL_Surface *newImage = NULL;
+  SDL_Surface *retImage = NULL;
+
+  newImage = SDL_LoadBMP(filename.c_str());
+
+  if(newImage != NULL){
+    retImage = SDL_DisplayFormat(newImage);
+    SDL_FreeSurface(newImage);
+  }
+
+  return retImage;
+}
+
+/*Blits the surface to the destination surface at pos (x,y)*/
+void applySurface(int x, int y, SDL_Surface *fromS, SDL_Surface *toS){
+  SDL_Rect offset;
+  offset.x = x;
+  offset.y = y;
+
+  SDL_BlitSurface(fromS, NULL, toS, &offset);
+}
---
+++ aux.hh
@@ -0,0 +1,12 @@
+#ifndef _inc_aux_ 
+#define _inc_aux_ 
+
+#include <string>
+#include "SDL/SDL.h"
+
+using namespace std;
+
+SDL_Surface *loadImage(string filename);
+void applySurface(int x, int y, SDL_Surface *fromS, SDL_Surface *toS);
+
+#endif
