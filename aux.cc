f1dabd42982b2d4d230bc285b708c9f6c3f84f19
Weierstrass <andreas@bulling.se>
2012-03-11 14:41:24 +0100

    add clear and overwrite surface routines

--- aux.cc
+++ aux.cc
@@ -36,6 +36,18 @@ void applySurface(int x, int y, SDL_Surface *fromS, SDL_Surface *toS){
   SDL_BlitSurface(fromS, NULL, toS, &offset);
 }
 
+/*Overwrite surface with new one*/
+
+void overWriteSurface(SDL_Surface *fromS, SDL_Surface *toS){
+  clearSurface(toS);
+  applySurface(0, 0, fromS, toS);
+}
+
+void clearSurface(SDL_Surface *surf){
+  SDL_FillRect(surf, NULL,
+	       SDL_MapRGB(surf->format, 0, 0, 0));  
+}
+
 /*returns the width of the current window*/
 int getWindowWidth(){
   if(SDL_GetVideoSurface() == NULL){
--- aux.hh
+++ aux.hh
@@ -5,12 +5,15 @@ using namespace std;
 
 #include <string>
 #include "SDL/SDL.h"
+#include "SDL/SDL_ttf.h"
 #include "const.hh"
 
 
 SDL_Surface *loadImage(string filename);
 void applySurface(int x, int y, SDL_Surface *fromS, SDL_Surface *toS);
+void overWriteSurface(SDL_Surface *fromS, SDL_Surface *toS);
 SDL_Surface *createSurface(int w, int h);
+void clearSurface(SDL_Surface *surf);
 int getWindowWidth();
 int getWindowHeight();
 #endif
